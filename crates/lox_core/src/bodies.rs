5164079d4cc6f97182c420cffbd66df0319fbd5e
Helge Eichhorn <git@helgeeichhorn.de>
2023-03-14 22:43:32 +0100

    Prototype API

--- crates/lox_core/src/bodies.rs
+++ crates/lox_core/src/bodies.rs
@@ -1,9 +1,5 @@
-use crate::bodies::pck_constants::BODY10_RADII;
-
 pub mod barycenters;
 pub mod minor;
-#[allow(clippy::approx_constant, clippy::excessive_precision, dead_code)]
-pub mod pck_constants;
 pub mod planets;
 pub mod satellites;
 
@@ -11,11 +7,19 @@ pub trait NaifId {
     fn id() -> i32;
 }
 
+pub fn naif_id<T: NaifId>(_: T) -> i32 {
+    <T as NaifId>::id()
+}
+
 pub trait Ellipsoid {
     fn max_equatorial_radius() -> f64;
     fn min_equatorial_radius() -> f64;
     fn polar_radius() -> f64;
     fn mean_radius() -> f64;
+}
+
+pub fn mean_radius<T: Ellipsoid>(_: T) -> f64 {
+    <T as Ellipsoid>::mean_radius()
 }
 
 pub struct Sun;
@@ -26,18 +30,13 @@ impl NaifId for Sun {
     }
 }
 
-impl Ellipsoid for Sun {
-    fn max_equatorial_radius() -> f64 {
-        BODY10_RADII[0]
-    }
-    fn min_equatorial_radius() -> f64 {
-        BODY10_RADII[1]
-    }
-    fn polar_radius() -> f64 {
-        BODY10_RADII[2]
-    }
-    fn mean_radius() -> f64 {
-        let sum: f64 = BODY10_RADII.iter().sum();
-        sum / 3.0
+#[cfg(test)]
+mod tests {
+    use super::planets::Earth;
+    use super::*;
+
+    #[test]
+    fn test_mean_radius() {
+        assert_eq!(mean_radius(Earth), 6371.008366666666);
     }
 }
