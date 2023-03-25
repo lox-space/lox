7b2a7f42a2faa239f61d6df08a15ba3562bdee31
Helge Eichhorn <git@helgeeichhorn.de>
2023-03-25 21:47:11 +0100

    Add ISS orbit example

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,1 +1,8 @@
-pub use lox_core::bodies::barycenters::SolarSystemBarycenter;
+pub use lox_core::bodies::barycenters::*;
+pub use lox_core::bodies::planets::*;
+pub use lox_core::bodies::{Ellipsoid, PointMass, Spheroid, TriAxial};
+
+pub use lox_core::time::dates::*;
+pub use lox_core::time::epochs::*;
+
+pub use lox_core::two_body::{Cartesian, DVec3, Keplerian, TwoBody};
