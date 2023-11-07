bc92afa9fd12470e5cab488452ccf6a64ca32d64
AngusGMorrison <github@angus-morrison.com>
2023-11-07 08:30:05 +0000

    Add NaifId trait bound to BodyRotationalElements

--- crates/lox_core/src/bodies.rs
+++ crates/lox_core/src/bodies.rs
@@ -65,7 +65,7 @@ pub fn gravitational_parameter<T: PointMass>(_: T) -> f64 {
 /// Semantic type alias for f64 in the context of polynomial types and operations.
 pub type PolynomialCoefficient = f64;
 
-pub trait BodyRotationalElements: Copy {
+pub trait BodyRotationalElements: NaifId {
     const RIGHT_ASCENSION_COEFFICIENTS: [PolynomialCoefficient; 3];
     const DECLINATION_COEFFICIENTS: [PolynomialCoefficient; 3];
     const PRIME_MERIDIAN_COEFFICIENTS: [PolynomialCoefficient; 3];
