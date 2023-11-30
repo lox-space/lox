c5f21bceb1162daa47d3dc98c04fd16e5c0c640d
Andrei Zisu <matzipan@gmail.com>
2023-11-30 23:22:25 +0100

    Add more tests to utc tai conversion

--- crates/lox_core/src/time/leap_seconds/api.rs
+++ crates/lox_core/src/time/leap_seconds/api.rs
@@ -194,4 +194,74 @@ pub mod test {
             Ok(37.0)
         );
     }
+
+    #[test]
+    fn test_range_warnings() {
+        // Values validated against LeapSeconds.jl
+
+        // datetime2julian(DateTime(1959, 1, 1))
+        assert_eq!(
+            offset_tai_utc(&TwoPartDateTime::from((2.4365695e6, 0f64))),
+            Err(LeapSecondError::UTCDateBefore1960)
+        );
+        // datetime2julian(DateTime(1959, 1, 1))
+        assert_eq!(
+            offset_utc_tai(&TwoPartDateTime::from((2.4365695e6, 0f64))),
+            Err(LeapSecondError::UTCDateBefore1960)
+        );
+    }
+
+    #[test]
+    fn test_during_leap_seconds() {
+        let parameters: Vec<(TwoPartDateTime, TwoPartDateTime)> = vec![
+            (
+                // dt = (1963, 7, 23, 14, 12, 3.0)
+                // utc = ERFA.dtf2d("UTC", dt...)
+                TwoPartDateTime::from((2.4382335e6, 0.5917013888888889)),
+                // tai = ERFA.utctai(utc...)
+                TwoPartDateTime::from((2.4382335e6, 0.5917301446782292)),
+            ),
+            (
+                // dt = (2012, 6, 30, 23, 59, 59.0)
+                // utc = ERFA.dtf2d("UTC", dt...)
+                TwoPartDateTime::from((2.4561085e6, 0.9999768521197672)),
+                // tai = ERFA.utctai(utc...)
+                TwoPartDateTime::from((2.4561085e6, 1.0003819444444444)),
+            ),
+            (
+                // dt = (2012, 6, 30, 23, 59, 60.0)
+                // utc = ERFA.dtf2d("UTC", dt...)
+                TwoPartDateTime::from((2.4561085e6, 0.9999884260598836)),
+                // tai = ERFA.utctai(utc...)
+                TwoPartDateTime::from((2.4561085e6, 1.0003935185185184)),
+            ),
+            (
+                // dt = (2012, 6, 30, 23, 59, 60.5)
+                // utc = ERFA.dtf2d("UTC", dt...)
+                TwoPartDateTime::from((2.4561085e6, 0.9999942130299417)),
+                // tai = ERFA.utctai(utc...)
+                TwoPartDateTime::from((2.4561085e6, 1.0003993055555553)),
+            ),
+            (
+                // dt = (2012, 7, 1, 0, 0, 0.0)
+                // utc = ERFA.dtf2d("UTC", dt...)
+                TwoPartDateTime::from((2.4561095e6, 0.0)),
+                // tai = ERFA.utctai(utc...)
+                TwoPartDateTime::from((2.4561095e6, 0.0004050925925925926)),
+            ),
+        ];
+
+        for parameter in parameters {
+            let diff_utc_tai = offset_utc_tai(&parameter.0).unwrap() / SECONDS_PER_DAY;
+            let diff_tai_utc = offset_tai_utc(&parameter.1).unwrap() / SECONDS_PER_DAY;
+
+            assert!((diff_utc_tai.abs() - diff_tai_utc.abs()).abs() < 1e-14);
+
+            let utc_jd = parameter.0.day + parameter.0.seconds_offset;
+            let tai_jd = parameter.1.day + parameter.1.seconds_offset;
+
+            assert!((utc_jd - diff_utc_tai - tai_jd).abs() < 1e-9);
+            assert!((tai_jd - diff_tai_utc - utc_jd).abs() < 1e-9);
+        }
+    }
 }
