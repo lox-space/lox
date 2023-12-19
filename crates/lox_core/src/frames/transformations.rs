c73ab8536766bf3189828226e16a29d6fa0aa794
AngusGMorrison <github@angus-morrison.com>
2023-12-19 19:08:30 +0000

    Passing tests

--- crates/lox_core/src/frames/transformations.rs
+++ crates/lox_core/src/frames/transformations.rs
@@ -9,10 +9,13 @@
 use crate::types::Radians;
 use glam::DMat3;
 
-// TODO: Decide on correct type once package structure discussed with Helge.
-type XY = (f64, f64);
+// TODO: Decide on correct home for this type once module structure discussed with
+// Helge. (An array is preferable to a tuple because the CIP calculation requires the dynamic
+// access an array provides, and we want to avoid excess allocations moving between steps of the
+// pipeline.)
+type XY = [f64; 2];
 
-// The spherical angles E and d.
+/// The spherical angles E and d.
 struct SphericalAngles {
     e: Radians,
     d: Radians,
@@ -20,8 +23,8 @@ struct SphericalAngles {
 
 impl SphericalAngles {
     fn new(cip: XY) -> Self {
-        let r2 = cip.0 * cip.0 + cip.1 * cip.1;
-        let e = cip.0.atan2(cip.1);
+        let r2 = cip[0] * cip[0] + cip[1] * cip[1];
+        let e = cip[1].atan2(cip[0]);
         let d = (r2 / (1.0 - r2)).sqrt().atan();
         Self { e, d }
     }
@@ -30,12 +33,15 @@ impl SphericalAngles {
 /// Compute the celestial to intermediate-frame-of-date matrix given the CIP (X, Y) coordinates
 /// and the CIO locator, s. This matrix is the first step in transforming CRS to
 /// TRS coordinates.
+///
+/// Note that the signs of all angles are reversed relative to ERFA, which uses left-handed
+/// coordinates, whereas glam is right-handed.
 pub fn celestial_to_intermediate_frame_of_date_matrix(cip: XY, s: Radians) -> DMat3 {
     let spherical_angles = SphericalAngles::new(cip);
     let mut result = DMat3::default();
-    result = DMat3::from_rotation_z(spherical_angles.e) * result;
+    result = DMat3::from_rotation_z(-spherical_angles.e) * result;
     result = DMat3::from_rotation_y(-spherical_angles.d) * result;
-    DMat3::from_rotation_z(-(spherical_angles.e + s)) * result
+    DMat3::from_rotation_z(spherical_angles.e + s) * result
 }
 
 #[cfg(test)]
@@ -43,13 +49,14 @@ mod tests {
     use super::*;
     use float_eq::assert_float_eq;
 
-    const TOLERANCE: f64 = 1e-11;
+    // TODO: Is this sufficient?
+    const TOLERANCE: f64 = 1e-9;
 
     #[test]
     fn test_celestial_to_intermediate_frame_of_date_matrix_jd0() {
-        let mut cip = (-0.4088355637476968, -0.38359667445777073);
-        let mut s = -0.0723985415686306;
-        let mut expected = &[
+        let cip = [-0.4088355637476968, -0.38359667445777073];
+        let s = -0.0723985415686306;
+        let expected = [
             0.899981235912944,
             -0.151285348992267,
             -0.408835563747697,
@@ -58,17 +65,62 @@ mod tests {
             -0.383596674457771,
             0.435512150790498,
             0.353018545339750,
-            0.828074316206005,
+            0.8280743162060046,
         ];
-        let mut actual = celestial_to_intermediate_frame_of_date_matrix(cip, s).to_cols_array();
+        let actual = celestial_to_intermediate_frame_of_date_matrix(cip, s).to_cols_array();
+        assert_mat3_eq(&expected, &actual)
+    }
+
+    #[test]
+    fn test_celestial_to_intermediate_frame_of_date_matrix_j2000() {
+        let cip = [-0.0000269463795685740, -0.00002800472282281282];
+        let s = -0.00000001013396519178;
+        let expected = [
+            0.999999999636946,
+            -0.00000001051127817488,
+            -0.000026946379569,
+            0.00000000975665225778,
+            0.999999999607868,
+            -0.000028004722823,
+            0.000026946379852,
+            0.000028004722550,
+            0.999999999244814,
+        ];
+        let actual = celestial_to_intermediate_frame_of_date_matrix(cip, s).to_cols_array();
+        assert_mat3_eq(&expected, &actual)
+    }
+
+    #[test]
+    fn test_celestial_to_intermediate_frame_of_date_matrix_j2100() {
+        let cip = [0.00972070446172924, -0.0000673058699616719];
+        let s = -0.00000000480511934533;
+        let expected = [
+            0.999952752836184,
+            0.00000032233307144280,
+            0.009720704461729,
+            0.00000033194308309287,
+            0.999999997734904,
+            -0.00006730586996167191,
+            -0.009720704461405,
+            0.00006730591667081695,
+            0.999952750571089,
+        ];
+        let actual = celestial_to_intermediate_frame_of_date_matrix(cip, s).to_cols_array();
+        assert_mat3_eq(&expected, &actual)
+    }
+
+    fn assert_mat3_eq(expected: &[f64; 9], actual: &[f64; 9]) {
         for i in 0..9 {
             assert_float_eq!(
                 expected[i],
                 actual[i],
-                rel <= TOLERANCE,
-                "\nexpected:\t{}\nactual:\t{}",
-                DMat3::from_cols_array(&actual),
-                DMat3::from_cols_array(&expected),
+                abs <= TOLERANCE,
+                "actual matrix differed from expected matrix at col {}, row {}:\n\t\
+                    expected: {},\n\tactual: {}",
+                i / 3,
+                i % 3,
+                DMat3::from_cols_array(expected),
+                DMat3::from_cols_array(actual),
             );
         }
     }
