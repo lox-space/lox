f288785ef2e7b2945e99279e1c414bec63cfc66e
Helge Eichhorn <git@helgeeichhorn.de>
2024-01-24 14:43:46 +0100

    Add anomaly tests

--- crates/lox_core/src/coords/anomalies.rs
+++ crates/lox_core/src/coords/anomalies.rs
@@ -15,3 +15,30 @@ pub fn eccentric_to_true(eccentric_anomaly: f64, eccentricity: f64) -> f64 {
     2.0 * (((1.0 + eccentricity) / (1.0 - eccentricity)).sqrt() * (eccentric_anomaly / 2.0).tan())
         .atan()
 }
+
+#[cfg(test)]
+mod tests {
+    use std::f64::consts::PI;
+
+    use float_eq::assert_float_eq;
+
+    use super::*;
+
+    #[test]
+    fn test_hyperbolic() {
+        assert_float_eq!(
+            hyperbolic_to_true(PI / 2.0, 1.2),
+            2.2797028138935547,
+            rel <= 1e-8
+        );
+    }
+
+    #[test]
+    fn test_eccentric() {
+        assert_float_eq!(
+            eccentric_to_true(PI / 2.0, 0.2),
+            1.7721542475852272,
+            rel <= 1e-8
+        );
+    }
+}
