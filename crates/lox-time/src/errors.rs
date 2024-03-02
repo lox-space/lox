f24f97ecc605b9c450ab429cea70816734ef2ec7
AngusGMorrison <github@angus-morrison.com>
2024-03-02 18:57:06 +0000

    Fix and test LoxTimeError Eq impl

--- crates/lox-time/src/errors.rs
+++ crates/lox-time/src/errors.rs
@@ -31,9 +31,8 @@ impl PartialEq for LoxTimeError {
         use LoxTimeError::*;
 
         match (self, other) {
-            (InvalidDate(_, _, _), InvalidDate(_, _, _))
-            | (InvalidTime(_, _, _), InvalidTime(_, _, _))
-            | (NonLeapYear, NonLeapYear) => self == other,
+            (InvalidDate(y1, m1, d1), InvalidDate(y2, m2, d2)) => y1 == y2 && m1 == m2 && d1 == d2,
+            (InvalidTime(h1, m1, s1), InvalidTime(h2, m2, s2)) => h1 == h2 && m1 == m2 && s1 == s2,
             (InvalidSeconds(f1), InvalidSeconds(f2))
             | (InvalidSubsecond(f1), InvalidSubsecond(f2)) => {
                 if f1.is_nan() && f2.is_nan() {
@@ -42,6 +41,7 @@ impl PartialEq for LoxTimeError {
 
                 f1 == f2
             }
+            (NonLeapYear, NonLeapYear) => true,
             (
                 InvalidTimeDelta {
                     raw: r1,
@@ -64,5 +64,42 @@ impl PartialEq for LoxTimeError {
             }
             _ => false,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::LoxTimeError::*;
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(InvalidDate(2024, 2, 30), InvalidDate(2024, 2, 30), true)]
+    #[case(InvalidDate(2024, 2, 30), InvalidDate(2024, 2, 29), false)]
+    #[case(InvalidDate(2024, 2, 30), NonLeapYear, false)]
+    #[case(InvalidTime(23, 59, 60), InvalidTime(23, 59, 60), true)]
+    #[case(InvalidTime(23, 59, 60), InvalidTime(23, 59, 59), false)]
+    #[case(InvalidTime(23, 59, 60), NonLeapYear, false)]
+    #[case(NonLeapYear, NonLeapYear, true)]
+    #[case(NonLeapYear, InvalidDate(2024, 2, 30), false)]
+    #[case(InvalidSeconds(60.0), InvalidSeconds(60.0), true)]
+    #[case(InvalidSeconds(f64::NAN), InvalidSeconds(f64::NAN), true)]
+    #[case(InvalidSeconds(60.0), InvalidSeconds(59.0), false)]
+    #[case(InvalidSeconds(60.0), NonLeapYear, false)]
+    #[case(InvalidSubsecond(1.0), InvalidSubsecond(1.0), true)]
+    #[case(InvalidSubsecond(f64::NAN), InvalidSubsecond(f64::NAN), true)]
+    #[case(InvalidSubsecond(1.0), InvalidSubsecond(0.0), false)]
+    #[case(InvalidSubsecond(1.0), NonLeapYear, false)]
+    #[case(InvalidTimeDelta { raw: 1.0, detail: "foo".to_string() }, InvalidTimeDelta { raw: 1.0, detail: "foo".to_string() }, true)]
+    #[case(InvalidTimeDelta { raw: f64::NAN, detail: "foo".to_string() }, InvalidTimeDelta { raw: f64::NAN, detail: "foo".to_string() }, true)]
+    #[case(InvalidTimeDelta { raw: 1.0, detail: "foo".to_string() }, InvalidTimeDelta { raw: 2.0, detail: "foo".to_string() }, false)]
+    #[case(InvalidTimeDelta { raw: 1.0, detail: "foo".to_string() }, InvalidTimeDelta { raw: 1.0, detail: "bar".to_string() }, false)]
+    #[case(InvalidTimeDelta { raw: 1.0, detail: "foo".to_string() }, NonLeapYear, false)]
+    fn test_lox_time_error_eq(
+        #[case] lhs: LoxTimeError,
+        #[case] rhs: LoxTimeError,
+        #[case] expected: bool,
+    ) {
+        assert_eq!(expected, lhs == rhs);
     }
 }
