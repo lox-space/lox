d70e6fb453f780c0e7d42475b9f99810e847749c
Andrei Zisu <matzipan@gmail.com>
2024-03-22 00:17:17 +0100

    Expand test and add support for string numbers

--- crates/lox-utils/src/ndm/json/omm.rs
+++ crates/lox-utils/src/ndm/json/omm.rs
@@ -6,22 +6,36 @@
  * file, you can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+use serde_aux::prelude::*;
 use std::collections::HashMap;
-
-use serde;
 
 #[derive(Clone, Debug, Default, PartialEq, serde::Deserialize, serde::Serialize)]
 #[serde(default)]
 pub struct SpacecraftParametersType {
-    #[serde(rename = "MASS")]
+    #[serde(
+        rename = "MASS",
+        deserialize_with = "deserialize_option_number_from_string"
+    )]
     pub mass: Option<f64>,
-    #[serde(rename = "SOLAR_RAD_AREA")]
+    #[serde(
+        rename = "SOLAR_RAD_AREA",
+        deserialize_with = "deserialize_option_number_from_string"
+    )]
     pub solar_rad_area: Option<f64>,
-    #[serde(rename = "SOLAR_RAD_COEFF")]
+    #[serde(
+        rename = "SOLAR_RAD_COEFF",
+        deserialize_with = "deserialize_option_number_from_string"
+    )]
     pub solar_rad_coeff: Option<f64>,
-    #[serde(rename = "DRAG_AREA")]
+    #[serde(
+        rename = "DRAG_AREA",
+        deserialize_with = "deserialize_option_number_from_string"
+    )]
     pub drag_area: Option<f64>,
-    #[serde(rename = "DRAG_COEFF")]
+    #[serde(
+        rename = "DRAG_COEFF",
+        deserialize_with = "deserialize_option_number_from_string"
+    )]
     pub drag_coeff: Option<f64>,
 }
 
@@ -32,44 +46,92 @@ pub struct MeanElementsType {
     pub epoch: String,
     #[serde(rename = "SEMI_MAJOR_AXIS")]
     pub semi_major_axis: Option<f64>,
-    #[serde(rename = "MEAN_MOTION")]
+    #[serde(
+        rename = "MEAN_MOTION",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub mean_motion: f64,
-    #[serde(rename = "ECCENTRICITY")]
+    #[serde(
+        rename = "ECCENTRICITY",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub eccentricity: f64,
-    #[serde(rename = "INCLINATION")]
+    #[serde(
+        rename = "INCLINATION",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub inclination: f64,
-    #[serde(rename = "RA_OF_ASC_NODE")]
+    #[serde(
+        rename = "RA_OF_ASC_NODE",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub ra_of_asc_node: f64,
-    #[serde(rename = "ARG_OF_PERICENTER")]
+    #[serde(
+        rename = "ARG_OF_PERICENTER",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub arg_of_pericenter: f64,
-    #[serde(rename = "MEAN_ANOMALY")]
+    #[serde(
+        rename = "MEAN_ANOMALY",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub mean_anomaly: f64,
-    #[serde(rename = "GM")]
+    #[serde(
+        rename = "GM",
+        deserialize_with = "deserialize_option_number_from_string"
+    )]
     pub gm: Option<f64>,
 }
 
 #[derive(Clone, Debug, Default, PartialEq, serde::Deserialize, serde::Serialize)]
 #[serde(default)]
 pub struct TleParametersType {
-    #[serde(rename = "EPHEMERIS_TYPE")]
+    #[serde(
+        rename = "EPHEMERIS_TYPE",
+        deserialize_with = "deserialize_option_number_from_string"
+    )]
     pub ephemeris_type: Option<i32>,
     #[serde(rename = "CLASSIFICATION_TYPE")]
     pub classification_type: Option<String>,
-    #[serde(rename = "NORAD_CAT_ID")]
+    #[serde(
+        rename = "NORAD_CAT_ID",
+        deserialize_with = "deserialize_option_number_from_string"
+    )]
     pub norad_cat_id: Option<i32>,
-    #[serde(rename = "ELEMENT_SET_NO")]
+    #[serde(
+        rename = "ELEMENT_SET_NO",
+        deserialize_with = "deserialize_option_number_from_string"
+    )]
     pub element_set_no: Option<i64>,
-    #[serde(rename = "REV_AT_EPOCH")]
+    #[serde(
+        rename = "REV_AT_EPOCH",
+        deserialize_with = "deserialize_option_number_from_string"
+    )]
     pub rev_at_epoch: Option<u64>,
-    #[serde(rename = "BSTAR")]
+    #[serde(
+        rename = "BSTAR",
+        deserialize_with = "deserialize_option_number_from_string"
+    )]
     pub bstar: Option<f64>,
-    #[serde(rename = "BTERM")]
+    #[serde(
+        rename = "BTERM",
+        deserialize_with = "deserialize_option_number_from_string"
+    )]
     pub bterm: Option<f64>,
-    #[serde(rename = "MEAN_MOTION_DOT")]
+    #[serde(
+        rename = "MEAN_MOTION_DOT",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub mean_motion_dot: f64,
-    #[serde(rename = "MEAN_MOTION_DDOT")]
+    #[serde(
+        rename = "MEAN_MOTION_DDOT",
+        deserialize_with = "deserialize_option_number_from_string"
+    )]
     pub mean_motion_ddot: Option<f64>,
-    #[serde(rename = "AGOM")]
+    #[serde(
+        rename = "AGOM",
+        deserialize_with = "deserialize_option_number_from_string"
+    )]
     pub agom: Option<f64>,
 }
 
@@ -78,47 +140,92 @@ pub struct TleParametersType {
 pub struct OpmCovarianceMatrixType {
     #[serde(rename = "COV_REF_FRAME")]
     pub cov_ref_frame: Option<String>,
-    #[serde(rename = "CX_X")]
+    #[serde(rename = "CX_X", deserialize_with = "deserialize_number_from_string")]
     pub cx_x: f64,
-    #[serde(rename = "CY_X")]
+    #[serde(rename = "CY_X", deserialize_with = "deserialize_number_from_string")]
     pub cy_x: f64,
-    #[serde(rename = "CY_Y")]
+    #[serde(rename = "CY_Y", deserialize_with = "deserialize_number_from_string")]
     pub cy_y: f64,
-    #[serde(rename = "CZ_X")]
+    #[serde(rename = "CZ_X", deserialize_with = "deserialize_number_from_string")]
     pub cz_x: f64,
-    #[serde(rename = "CZ_Y")]
+    #[serde(rename = "CZ_Y", deserialize_with = "deserialize_number_from_string")]
     pub cz_y: f64,
-    #[serde(rename = "CZ_Z")]
+    #[serde(rename = "CZ_Z", deserialize_with = "deserialize_number_from_string")]
     pub cz_z: f64,
-    #[serde(rename = "CX_DOT_X")]
+    #[serde(
+        rename = "CX_DOT_X",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub cx_dot_x: f64,
-    #[serde(rename = "CX_DOT_Y")]
+    #[serde(
+        rename = "CX_DOT_Y",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub cx_dot_y: f64,
-    #[serde(rename = "CX_DOT_Z")]
+    #[serde(
+        rename = "CX_DOT_Z",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub cx_dot_z: f64,
-    #[serde(rename = "CX_DOT_X_DOT")]
+    #[serde(
+        rename = "CX_DOT_X_DOT",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub cx_dot_x_dot: f64,
-    #[serde(rename = "CY_DOT_X")]
+    #[serde(
+        rename = "CY_DOT_X",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub cy_dot_x: f64,
-    #[serde(rename = "CY_DOT_Y")]
+    #[serde(
+        rename = "CY_DOT_Y",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub cy_dot_y: f64,
-    #[serde(rename = "CY_DOT_Z")]
+    #[serde(
+        rename = "CY_DOT_Z",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub cy_dot_z: f64,
-    #[serde(rename = "CY_DOT_X_DOT")]
+    #[serde(
+        rename = "CY_DOT_X_DOT",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub cy_dot_x_dot: f64,
-    #[serde(rename = "CY_DOT_Y_DOT")]
+    #[serde(
+        rename = "CY_DOT_Y_DOT",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub cy_dot_y_dot: f64,
-    #[serde(rename = "CZ_DOT_X")]
+    #[serde(
+        rename = "CZ_DOT_X",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub cz_dot_x: f64,
-    #[serde(rename = "CZ_DOT_Y")]
+    #[serde(
+        rename = "CZ_DOT_Y",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub cz_dot_y: f64,
-    #[serde(rename = "CZ_DOT_Z")]
+    #[serde(
+        rename = "CZ_DOT_Z",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub cz_dot_z: f64,
-    #[serde(rename = "CZ_DOT_X_DOT")]
+    #[serde(
+        rename = "CZ_DOT_X_DOT",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub cz_dot_x_dot: f64,
-    #[serde(rename = "CZ_DOT_Y_DOT")]
+    #[serde(
+        rename = "CZ_DOT_Y_DOT",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub cz_dot_y_dot: f64,
-    #[serde(rename = "CZ_DOT_Z_DOT")]
+    #[serde(
+        rename = "CZ_DOT_Z_DOT",
+        deserialize_with = "deserialize_number_from_string"
+    )]
     pub cz_dot_z_dot: f64,
 }
 
@@ -126,7 +233,7 @@ pub struct OpmCovarianceMatrixType {
 #[serde(default)]
 pub struct OmmMetadata {
     #[serde(rename = "COMMENT")]
-    pub comment_list: Vec<String>,
+    pub comment: Vec<String>,
     #[serde(rename = "OBJECT_NAME")]
     pub object_name: String,
     #[serde(rename = "OBJECT_ID")]
@@ -146,8 +253,8 @@ pub struct OmmMetadata {
 #[derive(Clone, Debug, Default, PartialEq, serde::Deserialize, serde::Serialize)]
 #[serde(default)]
 pub struct OdmHeader {
-    #[serde(rename = "COMMENT")] //@TODO
-    pub comment_list: Vec<String>,
+    #[serde(rename = "COMMENT")]
+    pub comment_list: String,
     #[serde(rename = "CLASSIFICATION")]
     pub classification_list: Vec<String>,
     #[serde(rename = "CREATION_DATE")]
@@ -177,7 +284,7 @@ pub struct OmmType {
     pub covariance_matrix: Option<OpmCovarianceMatrixType>,
 
     #[serde(flatten)]
-    pub user_defined_parameters: HashMap<String, String>,
+    pub user_defined_parameters: HashMap<String, Option<String>>,
 }
 
 mod test {
@@ -185,59 +292,82 @@ mod test {
 
     #[test]
     fn test_parse_json() {
-        let json = r#"
-     {
-         "OBJECT_NAME": "SORAYA",
-         "OBJECT_ID": "2024-015A",
-         "EPOCH": "2024-02-18T10:40:46.668576",
-         "MEAN_MOTION": 14.41921171,
-         "ECCENTRICITY": 0.0010889,
-         "INCLINATION": 64.5147,
-         "RA_OF_ASC_NODE": 28.0601,
-         "ARG_OF_PERICENTER": 260.5688,
-         "MEAN_ANOMALY": 99.4167,
-         "EPHEMERIS_TYPE": 0,
-         "CLASSIFICATION_TYPE": "U",
-         "NORAD_CAT_ID": 58817,
-         "ELEMENT_SET_NO": 999,
-         "REV_AT_EPOCH": 421,
-         "BSTAR": 0.00015073,
-         "MEAN_MOTION_DOT": 0.00000388,
-         "MEAN_MOTION_DDOT": 0,
-         "USER_DEFINED_FOO": "asd"
-     }"#;
+        let json = r#"[
+    {
+        "CCSDS_OMM_VERS": "2.0",
+        "COMMENT": "GENERATED VIA SPACE-TRACK.ORG API",
+        "CREATION_DATE": "2020-12-29T06:26:10",
+        "ORIGINATOR": "18 SPCS",
+        "OBJECT_NAME": "NUSAT-8 (MARIE)",
+        "OBJECT_ID": "2020-003C",
+        "CENTER_NAME": "EARTH",
+        "REF_FRAME": "TEME",
+        "TIME_SYSTEM": "UTC",
+        "MEAN_ELEMENT_THEORY": "SGP4",
+        "EPOCH": "2020-12-29T03:57:59.406624",
+        "MEAN_MOTION": "15.27989249",
+        "ECCENTRICITY": "0.00133560",
+        "INCLINATION": "97.2970",
+        "RA_OF_ASC_NODE": "66.4161",
+        "ARG_OF_PERICENTER": "110.6345",
+        "MEAN_ANOMALY": "334.7107",
+        "EPHEMERIS_TYPE": "0",
+        "CLASSIFICATION_TYPE": "U",
+        "NORAD_CAT_ID": "45018",
+        "ELEMENT_SET_NO": "999",
+        "REV_AT_EPOCH": "5327",
+        "BSTAR": "0.00008455300000",
+        "MEAN_MOTION_DOT": "0.00002241",
+        "MEAN_MOTION_DDOT": "0.0000000000000",
+        "SEMIMAJOR_AXIS": "6859.961",
+        "PERIOD": "94.242",
+        "APOAPSIS": "490.988",
+        "PERIAPSIS": "472.664",
+        "OBJECT_TYPE": "PAYLOAD",
+        "RCS_SIZE": "MEDIUM",
+        "COUNTRY_CODE": "ARGN",
+        "LAUNCH_DATE": "2020-01-15",
+        "SITE": "TSC",
+        "DECAY_DATE": null,
+        "FILE": "2911831",
+        "GP_ID": "168552672",
+        "TLE_LINE0": "0 NUSAT-8 (MARIE)",
+        "TLE_LINE1": "1 45018U 20003C   20364.16527091  .00002241  00000-0  84553-4 0  9997",
+        "TLE_LINE2": "2 45018  97.2970  66.4161 0013356 110.6345 334.7107 15.27989249 53274"
+    }
+]"#;
 
-        let message: OmmType = serde_json::de::from_str(json).unwrap();
+        let message: Vec<OmmType> = serde_json::de::from_str(json).unwrap();
 
         assert_eq!(
             message,
-            OmmType {
+            [OmmType {
                 header: OdmHeader {
-                    comment_list: vec![],
+                    comment_list: "GENERATED VIA SPACE-TRACK.ORG API".to_string(),
                     classification_list: vec![],
-                    creation_date: "".to_string(),
-                    originator: "".to_string(),
+                    creation_date: "2020-12-29T06:26:10".to_string(),
+                    originator: "18 SPCS".to_string(),
                     message_id: None,
                 },
                 metadata: OmmMetadata {
-                    comment_list: vec![],
-                    object_name: "SORAYA".to_string(),
-                    object_id: "2024-015A".to_string(),
-                    center_name: "".to_string(),
-                    ref_frame: "".to_string(),
+                    comment: vec![],
+                    object_name: "NUSAT-8 (MARIE)".to_string(),
+                    object_id: "2020-003C".to_string(),
+                    center_name: "EARTH".to_string(),
+                    ref_frame: "TEME".to_string(),
                     ref_frame_epoch: None,
-                    time_system: "".to_string(),
-                    mean_element_theory: "".to_string(),
+                    time_system: "UTC".to_string(),
+                    mean_element_theory: "SGP4".to_string(),
                 },
                 mean_elements: MeanElementsType {
-                    epoch: "2024-02-18T10:40:46.668576".to_string(),
+                    epoch: "2020-12-29T03:57:59.406624".to_string(),
                     semi_major_axis: None,
-                    mean_motion: 14.41921171,
-                    eccentricity: 0.0010889,
-                    inclination: 64.5147,
-                    ra_of_asc_node: 28.0601,
-                    arg_of_pericenter: 260.5688,
-                    mean_anomaly: 99.4167,
+                    mean_motion: 15.27989249,
+                    eccentricity: 0.0013356,
+                    inclination: 97.297,
+                    ra_of_asc_node: 66.4161,
+                    arg_of_pericenter: 110.6345,
+                    mean_anomaly: 334.7107,
                     gm: None,
                 },
                 spacecraft_parameters: Some(SpacecraftParametersType {
@@ -250,12 +380,12 @@ mod test {
                 tle_parameters: Some(TleParametersType {
                     ephemeris_type: Some(0,),
                     classification_type: Some("U".to_string(),),
-                    norad_cat_id: Some(58817,),
+                    norad_cat_id: Some(45018,),
                     element_set_no: Some(999,),
-                    rev_at_epoch: Some(421,),
-                    bstar: Some(0.00015073,),
+                    rev_at_epoch: Some(5327,),
+                    bstar: Some(8.4553e-5,),
                     bterm: None,
-                    mean_motion_dot: 3.88e-6,
+                    mean_motion_dot: 2.241e-5,
                     mean_motion_ddot: Some(0.0,),
                     agom: None,
                 },),
@@ -283,11 +413,40 @@ mod test {
                     cz_dot_y_dot: 0.0,
                     cz_dot_z_dot: 0.0,
                 },),
-                user_defined_parameters: HashMap::from([(
-                    "USER_DEFINED_FOO".to_string(),
-                    "asd".to_string()
-                )])
-            }
+                user_defined_parameters: HashMap::from([
+                    ("RCS_SIZE".to_string(), Some("MEDIUM".to_string(),)),
+                    (
+                        "TLE_LINE2".to_string(),
+                        Some(
+                            "2 45018  97.2970  66.4161 0013356 110.6345 334.7107 15.27989249 53274"
+                                .to_string(),
+                        )
+                    ),
+                    ("SITE".to_string(), Some("TSC".to_string(),)),
+                    ("LAUNCH_DATE".to_string(), Some("2020-01-15".to_string(),)),
+                    ("CCSDS_OMM_VERS".to_string(), Some("2.0".to_string(),)),
+                    ("DECAY_DATE".to_string(), None),
+                    ("FILE".to_string(), Some("2911831".to_string(),)),
+                    ("GP_ID".to_string(), Some("168552672".to_string(),)),
+                    ("OBJECT_TYPE".to_string(), Some("PAYLOAD".to_string(),)),
+                    ("APOAPSIS".to_string(), Some("490.988".to_string(),)),
+                    (
+                        "TLE_LINE0".to_string(),
+                        Some("0 NUSAT-8 (MARIE)".to_string(),)
+                    ),
+                    ("PERIOD".to_string(), Some("94.242".to_string(),)),
+                    ("SEMIMAJOR_AXIS".to_string(), Some("6859.961".to_string(),)),
+                    ("COUNTRY_CODE".to_string(), Some("ARGN".to_string(),)),
+                    (
+                        "TLE_LINE1".to_string(),
+                        Some(
+                            "1 45018U 20003C   20364.16527091  .00002241  00000-0  84553-4 0  9997"
+                                .to_string(),
+                        )
+                    ),
+                    ("PERIAPSIS".to_string(), Some("472.664".to_string(),)),
+                ]),
+            },]
         );
     }
 }
