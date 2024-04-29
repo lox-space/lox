4af142980352b860efcc4b88ed2b9d3b1a6b5f30
Andrei Zisu <matzipan@gmail.com>
2024-04-29 23:00:49 +0200

    Add copyright header

--- crates/lox-utils/src/ndm/kvn/opm.rs
+++ crates/lox-utils/src/ndm/kvn/opm.rs
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2023. Helge Eichhorn and the LOX contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 // KVN spec section 7.4 of https://public.ccsds.org/Pubs/502x0b3e1.pdf
 
 use nom::bytes::complete as nb;
