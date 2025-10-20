8a6f7fbca308be56bea467e2b4631e16f6cd76c9
Helge Eichhorn <git@helgeeichhorn.de>
2025-10-20 19:04:26 +0200

    feat(lox-test-utils): add ApproxEq trait

--- crates/lox-test-utils/src/lib.rs
+++ crates/lox-test-utils/src/lib.rs
@@ -1,5 +1,7 @@
 use std::path::PathBuf;
 
+pub mod approx_eq;
+
 /// Returns a [PathBuf] to the test fixture directory.
 pub fn data_dir() -> PathBuf {
     PathBuf::from(format!("{}/../../data", env!("CARGO_MANIFEST_DIR")))
