2f539837b23c60781ff76eadc6e262f809e85c76
Helge Eichhorn <git@helgeeichhorn.de>
2026-05-14 21:19:46 +0200

    feat(lox-odm): impl CI KVN format

--- crates/lox-odm/src/kvn/projection.rs
+++ crates/lox-odm/src/kvn/projection.rs
@@ -16,6 +16,7 @@ use crate::kvn::ast::{KvnEntry, KvnField};
 use crate::kvn::error::{KvnError, KvnErrorKind, Span};
 use crate::types::common::OdmTime;
 
+pub mod ci;
 pub mod oem;
 pub mod omm;
 pub mod opm;
